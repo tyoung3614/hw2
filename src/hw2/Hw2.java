d061e5c73901449c2989fd0ad3147dba13db8cfa
Tara Young <tyoung3614@gmail.com>
2022-07-09 20:36:16 -0400

    initial commit

---
+++ src/hw2/Hw2.java
@@ -0,0 +1,18 @@
+package hw2;
+
+public class Hw2 {
+
+	public static void main(String[] args) {
+
+		int apples = 0;
+
+		do {
+			System.out.println("amount of apples to buy:" + apples);
+
+			apples++;
+
+		} while (apples <= 3);
+
+	}
+
+}
